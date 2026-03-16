2232876009094f34143b650e12028846699b69e1
Third <rafi.34hack@gmail.com>
2026-03-16 10:48:02 +0700

    feat:quiz answer week 5

--- submissions/w05/answers.py
+++ submissions/w05/answers.py
@@ -1,9 +1,7 @@
 """Jawaban w05 — STUB (MAHASISWA)
-
 Aturan pengisian:
 - Implementasikan fungsi q01()..q12() sesuai soal di weeks/w05/quiz.qmd
 - Jangan ubah nama fungsi.
-
 Format jawaban:
 - T/F    -> bool  (True=Benar, False=Salah)
 - MC     -> str   ("A"/"B"/"C"/"D")
@@ -13,70 +11,54 @@ from __future__ import annotations
 def q01() -> bool:
     """[T/F] Nilai harapan dari sebuah variabel acak harus merupakan salah satu nilai yang
 mungkin muncul dari variabel tersebut."""
-    raise NotImplementedError
-
+    return False
 def q02() -> bool:
     """[T/F] Variansi dari sebuah variabel acak tidak pernah bernilai negatif."""
-    raise NotImplementedError
-
+    return True
 def q03() -> bool:
     """[T/F] Jika X adalah konstanta c, maka E[X] = c dan (X) = 0."""
-    raise NotImplementedError
-
+    return True
 def q04() -> str:
     """[MC] Jika E[X] = 5, maka E[2X+3] adalah:
-
 A) 10
 B) 13
 C) 8
 D) 5"""
-    raise NotImplementedError
-
+    return "B"
 def q05() -> str:
     """[MC] Variansi dari variabel acak X didefinisikan sebagai:
-
 A) E[X2]−(E[X])2
 B) E[X]−E[X2]
 C) E[X]
 D) E[X]2"""
-    raise NotImplementedError
-
+    return "A"
 def q06() -> str:
-    """[MC] Fungsi yang memberikan probabilitas P(X ) disebut:
-
+    """[MC] Fungsi yang memberikan probabilitas P(X) disebut:
 A) PMF.
 B) PDF.
 C) CDF.
 D) MGF."""
-    raise NotImplementedError
-
+    return "A"
 def q07() -> str:
     """[MC] Simpangan baku () adalah:
-
 A) Akar kuadrat dari variansi.
 B) Kuadrat dari mean.
-C) Selisih antara nilai maksimum dan minimum.
+ C) Selisih antara nilai maksimum dan minimum.
 D) Nilai harapan dari X."""
-    raise NotImplementedError
-
+    return "A"
 def q08() -> float:
     """[Numeric] Jika X memiliki nilai 0,1 dengan probabilitas 0,4,0,6, berapakah E[X]?"""
-    raise NotImplementedError
-
+    return 0.6
 def q09() -> float:
     """[Numeric] Hitung (X) untuk variabel pada soal nomor 8."""
-    raise NotImplementedError
-
+    return 0.24
 def q10() -> float:
     """[Numeric] Jika E[X] = 10 dan E[X2] = 116, berapakah variansinya?"""
-    raise NotImplementedError
-
+    return 16.0
 def q11() -> float:
     """[Numeric] Sebuah variabel acak memiliki nilai 1,2,3 dengan peluang sama. Berapakah
 nilai harapannya?"""
-    raise NotImplementedError
-
+    return 2.0
 def q12() -> float:
     """[Numeric] Jika (X) = 4, berapakah (3X+5)?"""
-    raise NotImplementedError
-
+    return 36.0
